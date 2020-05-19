0ab59d5ac431e283019eed97428ab94947add517
Maciej Krüger <mkg20001@gmail.com>
2020-05-19 18:02:10 +0200

    fix: cf request parameters

--- ipfs-dnslink-update.sh
+++ ipfs-dnslink-update.sh
@@ -12,7 +12,7 @@ die() {
 
 CF_API="https://api.cloudflare.com/client/v4"
 CF_CRED=(-H "X-Auth-Email: $CF_EMAIL" -H "X-Auth-Key: $CF_KEY" -H "Content-Type: application/json")
-CF_SEARCH="&status=active&page=1&per_page=20&order=status&direction=desc&match=all"
+CF_SEARCH="&status=active&page=1&per_page=20&order=name&direction=desc&match=all"
 
 PDNS_CRED=(-H "X-API-Key: $PDNS_KEY")
 
